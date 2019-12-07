f3bfd8e6b6658e02a43784ad83534b1510bc7c69
Andreas Amstutz <andreasamstutz@gmail.com>
2019-12-07 18:19:55 +0100

    Update to Mongo 4.2 db.aggregate()

--- mongo/queries/koeln.js
+++ mongo/queries/koeln.js
@@ -1,16 +1,20 @@
 // Datei: db/mongo/queries/koeln.js
-let dom = db.geoname.findOne({
-  name: /Köln.*Dom/
-});
-let res = db.runCommand({
-  geoNear: "geoname",
-  near: dom.location,
-  maxDistance: 1000,
-  spherical: true,
-  query: {
-    feature_code: 'HTL'
-  }
-})
-res.results.forEach(data => {
-  print(Math.round(data.dis)+ "m: " + data.obj.name);
+let dom = db.geoname.findOne({name: /Köln.*Dom/});
+let res = db.geoname.aggregate([
+  {
+    $geoNear: {
+       near: dom.location,
+       key: "location",
+       distanceField: "dist.calculated",
+       maxDistance: 1000,
+       query: { feature_code: 'HTL' },
+       spherical: true
+    }
+  },
+  { $limit: 10 },
+  { $sort : { "dist.calculated" : -1, name: 1} }
+])
+
+res._batch.forEach(data => {
+  print(Math.round(data.dist.calculated)+ "m: " + data.name);
 })
